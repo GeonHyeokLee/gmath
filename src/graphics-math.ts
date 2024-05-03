afde4ad097083298b2907929b19511e1a9598a8a
GeonHyeokLee <geonhyeoklee.kr@gmail.com>
2024-05-03 21:39:40 +0900

    add: cross function that only three dimension vector

--- src/graphics-math.ts
+++ src/graphics-math.ts
@@ -9,6 +9,10 @@ function compareVectorLength(vec1: Vector, vec2: Vector): boolean {
   return vec1.length > vec2.length;
 }
 
+function isThreeDimensionVector(vec1: Vector, vec2: Vector): boolean {
+  return vec1.length === 3 && vec2.length === 3;
+}
+
 // 두 벡터의 차원이 다를 경우 낮은 차원의 벡터를 기준으로 내적을 시도합니다.
 export function dot(vec1: Vector, vec2: Vector): Scalar {
   if (compareVectorLength(vec1, vec2)) {
@@ -18,6 +22,20 @@ export function dot(vec1: Vector, vec2: Vector): Scalar {
   return vec1.reduce((prev, cur, curIdx) => prev + cur * vec2[curIdx], 0);
 }
 
+// 3차원 벡터의 외적
+export function cross(vec1: Vector, vec2: Vector): Vector {
+  if (!isThreeDimensionVector(vec1, vec2)) {
+    throw new Error("It is not an three dimension vector.");
+  }
+
+  return [
+    vec1[1] * vec2[2] - vec2[1] * vec2[2],
+    vec1[2] * vec2[0] - vec2[2] * vec1[0],
+    vec1[0] * vec2[1] - vec2[0] - vec1[1],
+  ];
+}
+
+// 두 벡터의 정규화
 export function normalize(vec: Vector): Vector {
   const size = Math.sqrt(vec.reduce((prev, cur) => prev + cur * cur, 0));
 
